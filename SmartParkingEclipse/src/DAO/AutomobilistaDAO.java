db2f691bed674f0317a1fb644925910aca6ca838
ProPlayer96 <48991271+ProPlayer96@users.noreply.github.com>
2019-07-15 22:48:26 +0200

    classiDAO pt.2

--- SmartParkingEclipse/src/DAO/AutomobilistaDAO.java
+++ SmartParkingEclipse/src/DAO/AutomobilistaDAO.java
@@ -1,5 +1,9 @@
 package DAO;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class AutomobilistaDAO {
 	
 	
@@ -19,8 +23,26 @@ public class AutomobilistaDAO {
 		
 	}
 	
-	public static String readAutomobilista() {
-		return null;
+	public static int readContoAutomobilista(TransactionManager tm, String username, String password) throws SQLException {
+		int saldo = 0;
+		
+		tm.assertInTransaction();
+		try (PreparedStatement pt = tm.getConnection()
+				.prepareStatement("SELECT * FROM AUTOMOBILISTA WHERE ((USERNAME=?) (PASSWORD=?))")) {
+
+			pt.setString(1, username);
+			pt.setString(1, password);
+			try (ResultSet rs = pt.executeQuery()) {
+				if (rs.next() == true) {
+					saldo = rs.getInt("SALDO");
+				}
+			}
+
+			System.out.println("Salto residuo:" + saldo);
+		}
+		
+		
+		return saldo;
 		
 	}
 	
