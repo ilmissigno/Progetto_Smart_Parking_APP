45fc5333a50b7756908c7a7181c6e14f003cfffe
Raffaele Formisano <ilmissigno>
2019-07-29 17:07:15 +0200

    Proxy Skeleton finito e package diagram

---
+++ SmartParkingEclipse/src/Server/AvvioServer.java
@@ -0,0 +1,14 @@
+package Server;
+
+import Controller.GestoreSmartParking;
+
+public class AvvioServer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GestoreSmartParking.getInstance().avvia_skeleton();
+		//POSSO STESSO DA QUI AVVIARE GLI ALTRI GESTORI
+	}
+}
+
+
